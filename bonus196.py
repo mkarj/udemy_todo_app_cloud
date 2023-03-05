0437d51a1437ee52809494d46ac1ddb135c6495b
Mikko Karjalainen <mikkoaka@gmail.com>
2023-03-05 23:47:44 +0200

    Exercise 197 on bonus 196. Upload image and covert it to grayscale.

--- bonus196.py
+++ bonus196.py
@@ -2,6 +2,11 @@ import streamlit as st
 # pillow
 from PIL import Image
 
+st.subheader("Color to Grayscale Converter")
+
+# load picture
+uploaded_image = st.file_uploader("Upload Image")
+
 # start web cam
 with st.expander("Start Camera"):
     camera_image = st.camera_input("Camera")
@@ -10,9 +15,12 @@ with st.expander("Start Camera"):
 if camera_image:
     # create pillow image instance
     img = Image.open(camera_image)
-
     # convert the pillow image to grayscale
     gray_img = img.convert("L")
-
     # render/display grayscale image on the webpage
+    st.image(gray_img)
+
+if uploaded_image:
+    img = Image.open(uploaded_image)
+    gray_img = img.convert("L")
     st.image(gray_img)
